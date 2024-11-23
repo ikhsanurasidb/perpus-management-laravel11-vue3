8753ca3d53ccf9594ee4ed8d0156b53f539699e8
ikhsanurasidb <muhammad.nurasid@binus.ac.id>
2024-11-23 14:28:42 +0700

    change some config

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,10 +11,12 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /> --}}
 
         <!-- Styles / Scripts -->
-        @vite('resources/css/app.css')
+        {{-- @vite('resources/css/app.css') --}}
+        <link rel="stylesheet" href="https://perpus-management-laravel11-vue3-ikhsanurasids-projects.vercel.app/build/assets/app-Cj3Pe46D.css" />
+        <script type="module" src="https://perpus-management-laravel11-vue3-ikhsanurasids-projects.vercel.app/build/assets/app-B99DFDfs.js"></script>
     </head>
     <body>
         <main id="app"></main>
-        @vite('resources/js/app.js')
+        {{-- @vite('resources/js/app.js') --}}
     </body>
 </html>
