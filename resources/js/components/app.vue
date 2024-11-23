3f33a07133eba8573edfdf4ddf82ea6e75525752
ikhsanurasidb <muhammad.nurasid@binus.ac.id>
2024-11-23 12:33:46 +0700

    rename app.vue

--- resources/js/components/app.vue
+++ resources/js/components/app.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed } from "vue";
+import { computed } from "vue";
 import FooterView from "./FooterView.vue";
 import MenuView from "./MenuView.vue";
 import { useRoute } from "vue-router";
