21d0ec6a3df417c7b67cec594107b9dacb66f7b0
ikhsanurasidb <muhammad.nurasid@binus.ac.id>
2024-11-23 14:18:52 +0700

    change some config

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/build/{path}', function ($path) {
+    return response()->file(public_path("build/$path"));
+});
+
 Route::get('/{pathMatch}', function () {
     return view('welcome');
 })->where('pathMatch', '.*');
