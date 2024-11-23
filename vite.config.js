8d676ad743ba219c4d0099b99f5c829b10ad64f9
ikhsanurasidb <muhammad.nurasid@binus.ac.id>
2024-11-23 14:10:13 +0700

    change some config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,17 @@ import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+    server: {
+        https: true,
+    },
+    base:
+        process.env.NODE_ENV === "production"
+            ? "https://perpus-management-laravel11-vue3-ikhsanurasids-projects.vercel.app/"
+            : "/",
     resolve: {
         alias: {
-            '@': '/resources/js',
-          },
+            "@": "/resources/js",
+        },
     },
     plugins: [
         laravel({
