79964cd96dc75d2f8a4e4dc37330ac25f2f7ffcf
ikhsanurasidb <muhammad.nurasid@binus.ac.id>
2024-12-05 12:43:25 +0700

    edit vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,31 +3,35 @@ import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-    server: {
-        https: false,
-    },
-    base:
-        process.env.NODE_ENV === "production"
-            ? "/build/"
-            : "/",
-    resolve: {
-        alias: {
-            "@": "/resources/js",
-        },
-    },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
-        vue(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
+    resolve: {
+        alias: {
+            "@": "/resources/js",
+        },
+    },
     build: {
-        // Ensure proper MIME types are set
+        // Generate manifest.json in outDir
         manifest: true,
         rollupOptions: {
+            // Ensure to output asset files names with hash
             output: {
                 manualChunks: undefined,
+                entryFileNames: `assets/[name].[hash].js`,
+                chunkFileNames: `assets/[name].[hash].js`,
+                assetFileNames: `assets/[name].[hash].[ext]`
             },
         },
     },
