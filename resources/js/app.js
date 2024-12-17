949502f4ae9560f1fa2a2641bf6ddeb712eef777
ikhsanurasidb <muhammad.nurasid@binus.ac.id>
2024-12-17 11:30:44 +0700

    add pinia persisted state

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,12 @@
-import './bootstrap';
+import "./bootstrap";
 
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import App from './components/App.vue';
-import router from './router';
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./components/App.vue";
+import router from "./router";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
-createApp(App).use(router).use(createPinia()).mount('#app');
+createApp(App)
+    .use(router)
+    .use(createPinia().use(piniaPluginPersistedstate))
+    .mount("#app");
